27c3f4cc034a2f5aab4c0187dcf0cea836d27bb6
Shinya Nakajima <19910245+nakasho-dev@users.noreply.github.com>
2018-07-08 00:57:52 +0900

    StoryBoardなしの起動設定をした初回コミット

---
+++ SwiftAppCenterSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftAppCenterSample
+//
+//  Created by Shinya Nakajima on 2018/07/07.
+//  Copyright © 2018年 nakasho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.green
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
